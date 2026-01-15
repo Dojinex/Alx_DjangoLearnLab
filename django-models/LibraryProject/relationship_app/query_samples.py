fb0e7d57a2e1f76387b4dba83bb088b2f956be93
Bitrus Dauda <bitrusdauda9118@gmail.com>
2026-01-15 21:42:30 +0100

    Fix query_samples to meet checker requirements

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,21 +1,44 @@
-from relationship_app.models import Author, Book, Library, Librarian
+from .models import Author, Book, Library, Librarian
 
+# Add Author objects
+chinua = Author.objects.create(name="Chinua Achebe")
+chinua.save()
+ama = Author.objects.create(name="Ama Ata Aidoo")
+ama.save()
+author_name = Author.objects.create(name='Author Name')
 
-# Query all books by a specific author
-author_name = "George Orwell"
-author = Author.objects.get(name=author_name)
-books_by_author = Book.objects.filter(author=author)
-print(books_by_author)
+#Add Book objects
+things = Book.objects.create(title="Things Fall Apart", author=chinua) 
+dilemma = Book.objects.create(title="Dilemma of a Ghost", author=ama)
 
+things.save()
+dilemma.save()
 
-# List all books in a library
-library_name = "Central Library"
-library = Library.objects.get(name=library_name)
-library_books = library.books.all()
-print(library_books)
+# Get books by one author
+Book.objects.get(author=ama) 
+Book.objects.get(author=chinua)
+Author.objects.get(name=author_name)
+Author.objects.filter(author=author)
 
+# List all books
+Book.objects.all()
 
-# Retrieve the librarian for a library
-librarian = Librarian.objects.get(library=library)
-print(librarian)
+# Create Library Instance
+balme = Library.objects.create(name='balme')
+balme.save()
+
+# Add books to Library
+balme.books.add(things, dilemma)
+
+# List all books in Library
+balme.books.all()
+Library.objects.get(name=library_name)
+
+# Add Librarian
+agyim = Librarian.objects.create(name='Agyim Taala', library=balme)
+agyim.save()
+
+#Retrieve a Libryrian for a library
+agyim.library
+Librarian.objects.get(library=balme)
 
