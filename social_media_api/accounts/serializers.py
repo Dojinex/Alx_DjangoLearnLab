c6e144c0ae5ca920e80d314520497d5865b58900
Bitrus Dauda <bitrusdauda9118@gmail.com>
2026-02-16 20:03:00 +0100

    Fix registration serializer to use get_user_model for user creation

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 from .models import User
+from django.contrib.auth import get_user_model
+
 
 
 # Registration Serializer
@@ -9,13 +11,14 @@ class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = get_user_model().objects.create_user(**validated_data)
         Token.objects.create(user=user)
         return user
+
 
 # Login Serializer
 class LoginSerializer(serializers.Serializer):
@@ -33,3 +36,5 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
+
+# 
