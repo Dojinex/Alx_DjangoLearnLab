5e21b98ddcd4c58a810950df406897211c9b7fe8
Bitrus Dauda <bitrusdauda9118@gmail.com>
2026-02-16 15:55:58 +0100

    Create accounts app and custom user model

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+#Register account and rest_framework
     'accounts',
     'rest_framework',
 ]
@@ -117,6 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#Configure Media Files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+#Tell Django to use custom user
+AUTH_USER_MODEL = 'accounts.User'
+
 
