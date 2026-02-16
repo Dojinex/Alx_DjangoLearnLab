be4f3725f2479f38a7ae5fcca71cc76ec3ed1d3d
Bitrus Dauda <bitrusdauda9118@gmail.com>
2026-02-16 19:53:04 +0100

    commit user registration, login, and profile views

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from rest_framework.authtoken.models import Token
 
-# Create your views here.
+from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+
+# Register views
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = Token.objects.get(user=user)
+
+        return Response({
+            "user": serializer.data,
+            "token": token.key
+        })
+
+#Login views
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data
+        token, _ = Token.objects.get_or_create(user=user)
+
+        return Response({
+            "token": token.key
+        })
+
+# Profile views
+class ProfileView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+
