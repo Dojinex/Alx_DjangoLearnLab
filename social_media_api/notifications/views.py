590db88af205560052884787131ca359a008f9d1
Bitrus Dauda <bitrusdauda9118@gmail.com>
2026-02-21 20:46:32 +0100

    feat: implement notification system with list endpoint

--- social_media_api/notifications/views.py
+++ social_media_api/notifications/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Notification
+from .serializers import NotificationSerializer
 
-# Create your views here.
+
+
+
+class NotificationListView(generics.ListAPIView):
+    serializer_class = NotificationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Return notifications for the logged-in user
+        return self.request.user.notifications.all()
