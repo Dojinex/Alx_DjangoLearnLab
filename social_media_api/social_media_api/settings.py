d93ebcab6454230a896e2de77b71a5aa800dae73
Bitrus Dauda <bitrusdauda9118@gmail.com>
2026-02-22 22:53:39 +0100

    add production security settings for deployment

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -15,7 +15,7 @@ import os
 
 import dj_database_url
 
-
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -153,6 +153,17 @@ REST_FRAMEWORK = {
     ],
 }
 
+
+# =========================
+# SECURITY SETTINGS (PRODUCTION)
+# =========================
+
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
+
 X_FRAME_OPTIONS = "DENY"
